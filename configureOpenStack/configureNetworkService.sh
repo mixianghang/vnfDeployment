0f4fb9f03d847fbb7248e876a37d8293a0e88e11
xianghang mi <xmi@iu.edu>
2017-07-24 15:07:39 -0400

    fix

--- configureOpenStack/configureNetworkService.sh
+++ configureOpenStack/configureNetworkService.sh
@@ -4,5 +4,6 @@ hostIP=$(grep -E "$host" /etc/hosts | awk '{print $1}')
 echo $hostIP
 cp ./neutron.conf.template ./neutron.conf
 sed -e "s/IP_PlaceHolder/$hostIP/g" ./linuxbridge_agent.ini.template >linuxbridge_agent.ini
-scp ./neutron.conf ./linuxbridge_agent.ini root@$host:
+scp ./neutron.conf root@$host:
+scp ./linuxbridge_agent.ini root@$host:
 ./configureHostExpect.sh $host ./configureNeutron.sh
